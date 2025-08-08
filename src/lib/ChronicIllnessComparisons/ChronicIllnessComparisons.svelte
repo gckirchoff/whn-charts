8c8093be5e540a4079fbe9a2951be6029b125d61
NeoMar <150372772+MMar15@users.noreply.github.com>
2025-08-08 18:10:00 -0400

    Ratios up

--- src/lib/ChronicIllnessComparisons/ChronicIllnessComparisons.svelte
+++ src/lib/ChronicIllnessComparisons/ChronicIllnessComparisons.svelte
@@ -8,15 +8,40 @@
 		CsvPrevalenceData,
 		PrevalenceData
 	} from './constants';
+	import { rarityThreshold } from './DataChart/constants';
 
 	let { src }: ChronicIlnessComparisonsProps = $props();
 
-	let xProperty = $state('illness');
+	let xProperty = $state<keyof PrevalenceData>('illness');
 	let yProperty = $state<keyof PrevalenceData>('adultPrevalence');
 	let data = $state<PrevalenceData[]>([]);
 
+	let ratioed = $state(false);
+	const isRare = (prevalence: number) => prevalence < rarityThreshold;
+
+
+	let ratioData = $derived(() => {
+		if (!ratioed) return data;
+		return data.map((row) => {
+			let prev = row.adultPrevalence;
+			let search = row.relativeSearchInterest;
+
+			if (yProperty === 'adultPrevalence') {
+				return {
+					...row,
+					adultPrevalence: ((prev * 100) / search).toFixed(4),
+				};
+			} else {
+				return {
+					...row,
+					relativeSearchInterest: (search / (prev * 100)).toFixed(4),
+				};
+			}
+		});
+	});
+
 	onMount(async () => {
-		data = await csv<PrevalenceData>(src, (row) => {
+		const csvData = await csv<PrevalenceData>(src, (row) => {
 			const typedRow = row as CsvPrevalenceData;
 			console.log('Hello', typedRow);
 			return {
@@ -27,19 +52,43 @@
 				isPreventable: typedRow.isPreventable === 'TRUE',
 			};
 		});
+		data = csvData
+			.filter(({ adultPrevalence }) => !isRare(adultPrevalence))
+			.map((row) => ({ ...row, adultPrevalence: row.adultPrevalence * 100 }))
+			.sort((a, b) => (a.adultPrevalence > b.adultPrevalence ? 1 : -1));
 	});
 </script>
 
-<div>
-	<label>
-		Dependent y variable:
-		<select bind:value={yProperty}>
-			<option value="adultPrevalence"> adult prevalence </option>
-			<option value="relativeSearchInterest"> relative search interest</option>
-		</select>
-	</label>
+<div id="menu">
+	<div>
+		<label>
+			Dependent y variable:
+			<select bind:value={yProperty}>
+				<option value="adultPrevalence"> adult prevalence </option>
+				<option value="relativeSearchInterest"> relative search interest</option>
+			</select>
+		</label>
+	</div>
+
+	<div>
+		{#if yProperty === 'adultPrevalence'}
+			<label for="prev/search">View as Prevelance : Search Ratio?</label>
+			<input type="checkbox" id="prev/search" name="prev/search" value="p/s" bind:checked={ratioed}>
+		{:else if yProperty === 'relativeSearchInterest'}
+			<label for="search/prev">View as Search : Prevelance Ratio?</label>
+			<input type="checkbox" id="search/prev" name="search/prev" value="s/p" bind:checked={ratioed}>
+		{/if}
+	</div>
+</div>
+	
+<div style="height: 700px">
+	<DataChart data={ratioData()} {xProperty} yProperty={yProperty} />
 </div>
 
-<div style="height: 700px">
-	<DataChart {data} xProperty="illness" {yProperty} />
-</div>
+<style>
+	#menu {
+		display: flex;
+		justify-content: space-between;
+		font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+	}
+</style>
